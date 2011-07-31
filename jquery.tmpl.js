56c274d2bddb224fd7e2161ffbcc4620c5f1734a
Ferran Basora <fcsonline@gmail.com>
2011-07-31 16:08:12 +0200

    JQuery Template modification for datasource support. Now you can use <ul data-source="model.movies"> for bind all data to a DOM node, filling it with template

--- jquery.tmpl.js
+++ jquery.tmpl.js
@@ -482,3 +482,92 @@
 		jQuery( coll ).remove();
 	}
 })( jQuery );
+
+/* Ini datasource template extension */
+
+/*
+* object.watch v0.0.1: Cross-browser object.watch
+*
+* By Elijah Grey, http://eligrey.com
+*
+* A shim that partially implements object.watch and object.unwatch
+* in browsers that have accessor support.
+*
+* Public Domain.
+* NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+*/
+
+// object.watch
+if (!Object.prototype.watch)
+Object.prototype.watch = function (prop, handler) {
+    var oldval = this[prop], newval = oldval,
+    getter = function () {
+        return newval;
+    },
+    setter = function (val) {
+        oldval = newval;
+        return newval = handler.call(this, prop, oldval, val);
+    };
+    if (delete this[prop]) { // can't watch constants
+        if (Object.defineProperty) // ECMAScript 5
+        Object.defineProperty(this, prop, {
+            get: getter,
+            set: setter
+        });
+        else if (Object.prototype.__defineGetter__ && Object.prototype.__defineSetter__) { // legacy
+            Object.prototype.__defineGetter__.call(this, prop, getter);
+            Object.prototype.__defineSetter__.call(this, prop, setter);
+        }
+    }
+};
+
+// object.unwatch
+if (!Object.prototype.unwatch)
+Object.prototype.unwatch = function (prop) {
+    var val = this[prop];
+    delete this[prop]; // remove accessors
+    this[prop] = val;
+};
+
+(function($) {
+
+	$.extend($.fn, {
+		tmpldatasource: function(settings) {
+    
+            var listener = settings.listener;
+
+            if (listener == undefined) {
+                console.error ('Undefined model listener!')
+                return;
+            }
+
+            $.each(this, function(i, item){
+                var dsname = $(item).data('source');
+                var tmplname = $(item).data('template');
+
+                if (tmplname == undefined){
+                    tmplname = $(item).find('script').attr('id');
+                }
+
+                if (tmplname == undefined) {
+                    console.error ('Undefined template datasource!')
+                    return;
+                }
+
+                tmplname = '#' + tmplname;
+
+                settings['listener'].watch(dsname,
+                    function (id, oldval, newval) {
+                        $(item).children(':not(script)').remove();
+                        $(item).append($(tmplname).tmpl(newval));
+                        return newval;
+                    });
+                
+            });
+
+
+			return this;
+		}
+	});
+	
+})( jQuery );
